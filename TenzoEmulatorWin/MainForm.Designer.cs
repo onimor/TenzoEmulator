8f290a2984d2159d374b7c37ebe8bd0f05e885cb
onimor <onimor07@hotmail.com>
2025-12-11 13:31:24 +0300

    - Реализован win form вариант

---
+++ TenzoEmulatorWin/MainForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace TenzoEmulatorWin
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbPort = new ComboBox();
+            cmbBaud = new ComboBox();
+            numAddr = new NumericUpDown();
+            numSerial = new NumericUpDown();
+            numDecimals = new NumericUpDown();
+            numWeight = new NumericUpDown();
+            chkStable = new CheckBox();
+            chkNegative = new CheckBox();
+            chkOverload = new CheckBox();
+            btnStart = new Button();
+            btnStop = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            txtLog = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)numAddr).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numSerial).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numDecimals).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numWeight).BeginInit();
+            SuspendLayout();
+            // 
+            // cmbPort
+            // 
+            cmbPort.FormattingEnabled = true;
+            cmbPort.Items.AddRange(new object[] { "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "COM10", "COM11", "COM12", "COM13", "COM14" });
+            cmbPort.Location = new Point(12, 30);
+            cmbPort.Name = "cmbPort";
+            cmbPort.Size = new Size(151, 28);
+            cmbPort.TabIndex = 0;
+            // 
+            // cmbBaud
+            // 
+            cmbBaud.FormattingEnabled = true;
+            cmbBaud.Items.AddRange(new object[] { "110", "300", "600", "1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200", "230400", "460800", "921600" });
+            cmbBaud.Location = new Point(12, 101);
+            cmbBaud.Name = "cmbBaud";
+            cmbBaud.Size = new Size(151, 28);
+            cmbBaud.TabIndex = 1;
+            // 
+            // numAddr
+            // 
+            numAddr.Location = new Point(12, 177);
+            numAddr.Name = "numAddr";
+            numAddr.Size = new Size(150, 27);
+            numAddr.TabIndex = 2;
+            numAddr.ValueChanged += myAddr_ValueChanged;
+            // 
+            // numSerial
+            // 
+            numSerial.Location = new Point(210, 31);
+            numSerial.Name = "numSerial";
+            numSerial.Size = new Size(230, 27);
+            numSerial.TabIndex = 3;
+            numSerial.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numSerial.ValueChanged += numSerial_ValueChanged;
+            // 
+            // numDecimals
+            // 
+            numDecimals.Location = new Point(210, 177);
+            numDecimals.Maximum = new decimal(new int[] { 3, 0, 0, 0 });
+            numDecimals.Name = "numDecimals";
+            numDecimals.Size = new Size(230, 27);
+            numDecimals.TabIndex = 4;
+            numDecimals.Value = new decimal(new int[] { 2, 0, 0, 0 });
+            numDecimals.ValueChanged += numDecimals_ValueChanged;
+            // 
+            // numWeight
+            // 
+            numWeight.DecimalPlaces = 3;
+            numWeight.Location = new Point(210, 101);
+            numWeight.Maximum = new decimal(new int[] { 32000, 0, 0, 0 });
+            numWeight.Name = "numWeight";
+            numWeight.Size = new Size(230, 27);
+            numWeight.TabIndex = 5;
+            numWeight.Value = new decimal(new int[] { 12345, 0, 0, 131072 });
+            numWeight.ValueChanged += numWeight_ValueChanged;
+            // 
+            // chkStable
+            // 
+            chkStable.AutoSize = true;
+            chkStable.Checked = true;
+            chkStable.CheckState = CheckState.Checked;
+            chkStable.Location = new Point(489, 34);
+            chkStable.Name = "chkStable";
+            chkStable.Size = new Size(127, 24);
+            chkStable.TabIndex = 6;
+            chkStable.Text = "Стабильность";
+            chkStable.UseVisualStyleBackColor = true;
+            chkStable.CheckedChanged += chkStable_CheckedChanged;
+            // 
+            // chkNegative
+            // 
+            chkNegative.AutoSize = true;
+            chkNegative.Location = new Point(489, 128);
+            chkNegative.Name = "chkNegative";
+            chkNegative.Size = new Size(169, 24);
+            chkNegative.TabIndex = 7;
+            chkNegative.Text = "Отрицательный вес";
+            chkNegative.UseVisualStyleBackColor = true;
+            chkNegative.CheckedChanged += chkNegative_CheckedChanged;
+            // 
+            // chkOverload
+            // 
+            chkOverload.AutoSize = true;
+            chkOverload.Location = new Point(489, 78);
+            chkOverload.Name = "chkOverload";
+            chkOverload.Size = new Size(111, 24);
+            chkOverload.TabIndex = 8;
+            chkOverload.Text = "Перегрузка";
+            chkOverload.UseVisualStyleBackColor = true;
+            chkOverload.CheckedChanged += chkOverload_CheckedChanged;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(464, 409);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(94, 29);
+            btnStart.TabIndex = 9;
+            btnStart.Text = "Старт";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click_1;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(564, 409);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(94, 29);
+            btnStop.TabIndex = 10;
+            btnStop.Text = "Стоп";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 20);
+            label1.TabIndex = 11;
+            label1.Text = "COM порт";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 12;
+            label2.Text = "Скорость";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 154);
+            label3.Name = "label3";
+            label3.Size = new Size(137, 20);
+            label3.TabIndex = 13;
+            label3.Text = "Номер терминала";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(210, 8);
+            label4.Name = "label4";
+            label4.Size = new Size(132, 20);
+            label4.TabIndex = 14;
+            label4.Text = "Серийный номер";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(210, 78);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 20);
+            label5.TabIndex = 15;
+            label5.Text = "Вес";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(210, 154);
+            label6.Name = "label6";
+            label6.Size = new Size(230, 20);
+            label6.TabIndex = 16;
+            label6.Text = "Количество знаков после точки";
+            // 
+            // txtLog
+            // 
+            txtLog.Location = new Point(12, 238);
+            txtLog.Name = "txtLog";
+            txtLog.Size = new Size(645, 143);
+            txtLog.TabIndex = 17;
+            txtLog.Text = "";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(669, 450);
+            Controls.Add(txtLog);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Controls.Add(chkOverload);
+            Controls.Add(chkNegative);
+            Controls.Add(chkStable);
+            Controls.Add(numWeight);
+            Controls.Add(numDecimals);
+            Controls.Add(numSerial);
+            Controls.Add(numAddr);
+            Controls.Add(cmbBaud);
+            Controls.Add(cmbPort);
+            Name = "MainForm";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numAddr).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numSerial).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numDecimals).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numWeight).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbPort;
+        private ComboBox cmbBaud;
+        private NumericUpDown numAddr;
+        private NumericUpDown numSerial;
+        private NumericUpDown numDecimals;
+        private NumericUpDown numWeight;
+        private CheckBox chkStable;
+        private CheckBox chkNegative;
+        private CheckBox chkOverload;
+        private Button btnStart;
+        private Button btnStop;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private RichTextBox txtLog;
+    }
+}
---
+++ TenzoEmulatorWin/MainForm.cs
@@ -0,0 +1,313 @@
+using System.IO.Ports;
+
+namespace TenzoEmulatorWin
+{
+    public partial class MainForm : Form
+    {
+        private SerialPort port;
+        private Thread workerThread;
+        private volatile bool isRunning = false;
+
+        // Настройки эмулятора
+        private byte myAddr = 0x01;
+        private uint mySerial = 0x000001;
+        private double currentWeight = 123.45;
+        private int decimalPlaces = 2;
+        private int baudRate = 9600;
+        private bool isStable = true;
+        private bool isOverload = false;
+        private bool isNegative = false;
+        private string portName = "Com1";
+
+        private NotifyIcon trayIcon;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeTray();
+            LoadSettings();
+        }
+
+        private void InitializeTray()
+        {
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath),
+                Text = "Тензо-М Эмулятор",
+                Visible = true
+            };
+            trayIcon.DoubleClick += (s, e) => { this.Show(); this.WindowState = FormWindowState.Normal; };
+            trayIcon.ContextMenuStrip = new ContextMenuStrip();
+            //trayIcon.ContextMenuStrip.Items.Add("Открыть", null, (s, e) => trayIcon_DoubleClick(s, e));
+            trayIcon.ContextMenuStrip.Items.Add("Выход", null, (s, e) => Application.Exit());
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            cmbPort.Items.AddRange(SerialPort.GetPortNames());
+            if (cmbPort.Items.Count > 0) cmbPort.SelectedIndex = 0;
+
+            cmbBaud.SelectedIndex = cmbBaud.Items.IndexOf("9600");
+            numAddr.Value = myAddr;
+            numSerial.Value = mySerial;
+            numWeight.Value = (decimal)currentWeight;
+            numDecimals.Value = decimalPlaces;
+
+            chkStable.Checked = isStable;
+            chkOverload.Checked = isOverload;
+            chkNegative.Checked = isNegative;
+        }
+
+
+       
+
+        private void SerialWorker()
+        {
+            while (isRunning)
+            {
+                SerialPort sp = null;
+                try
+                {
+                    sp = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One)
+                    {
+                        ReadTimeout = 500,
+                        WriteTimeout = 500,
+                        RtsEnable = true,
+                        DtrEnable = true
+                    };
+                    sp.Open();
+                    LogInvoke($"[OK] {portName} подключён");
+
+                    while (isRunning && sp.IsOpen)
+                    {
+                        var frame = ReadFrame(sp);
+                        if (frame != null)
+                        {
+                            var resp = ProcessFrame(frame);
+                            if (resp != null)
+                                SendFrame(sp, resp);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogInvoke($"[ERR] {ex.Message}");
+                }
+                finally
+                {
+                    try { sp?.Close(); sp?.Dispose(); } catch { }
+                }
+
+                if (isRunning)
+                {
+                    LogInvoke("Переподключение через 1 сек...");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private byte[] ProcessFrame(byte[] payload)
+        {
+            if (payload.Length < 3) return null;
+
+            int pos = 0;
+            bool extended = payload[pos] == 0x00;
+            uint receivedSerial = 0;
+            byte addr = 0;
+
+            if (extended)
+            {
+                if (payload.Length < 4) return null;
+                receivedSerial = (uint)(payload[1] << 16 | payload[2] << 8 | payload[3]);
+                pos += 4;
+            }
+            else
+            {
+                addr = payload[0];
+                pos++;
+            }
+
+            byte cop = payload[pos++];
+
+            // Проверка адреса
+            if (extended && receivedSerial != mySerial) return null;
+            if (!extended && addr != myAddr) return null;
+
+            switch (cop)
+            {
+                case 0xA0: // Назначение адреса
+                    if (payload.Length >= pos + 1 && payload[pos] >= 1 && payload[pos] <= 0x9F)
+                    {
+                        myAddr = payload[pos];
+                        LogInvoke($"Новый адрес: 0x{myAddr:X2}");
+                        Invoke((Action)(() => numAddr.Value = myAddr));
+                    }
+                    return new byte[] { extended ? (byte)0 : myAddr, cop };
+
+                case 0xA1: // Серийный номер
+                    return new byte[] { 0x00, cop, (byte)(mySerial >> 16), (byte)(mySerial >> 8), (byte)mySerial };
+
+                case 0xC0: // Тарирование
+                    currentWeight = 0;
+                    Invoke((Action)(() => numWeight.Value = 0));
+                    return new byte[] { extended ? (byte)0 : myAddr, cop };
+
+                case 0xC2: // Нетто
+                case 0xC3: // Брутто
+                    return BuildWeightResponse(extended, cop);
+
+                default:
+                    return null;
+            }
+        }
+
+        private byte[] BuildWeightResponse(bool extended, byte cop)
+        {
+            long val = (long)Math.Abs(currentWeight * Math.Pow(10, decimalPlaces));
+            string s = val.ToString("D6");
+
+            byte w0 = (byte)((s[5] - '0') | ((s[4] - '0') << 4));
+            byte w1 = (byte)((s[3] - '0') | ((s[2] - '0') << 4));
+            byte w2 = (byte)((s[1] - '0') | ((s[0] - '0') << 4));
+
+            byte con = (byte)(
+                (isNegative ? 0x80 : 0) |
+                (isStable ? 0x10 : 0) |
+                (isOverload ? 0x08 : 0) |
+                (decimalPlaces & 0x03));
+
+            var resp = new List<byte>();
+            if (extended) resp.Add(0x00);
+            resp.Add(extended ? (byte)0 : myAddr);
+            resp.Add(cop);
+            resp.Add(w0); resp.Add(w1); resp.Add(w2); resp.Add(con);
+            return resp.ToArray();
+        }
+
+        private void SendFrame(SerialPort p, byte[] payload)
+        {
+            p.Write(new byte[] { 0xFF }, 0, 1);
+            byte crc = 0;
+            foreach (byte b in payload)
+            {
+                crc = CalcCrc(b, crc);
+                p.Write(new byte[] { b }, 0, 1);
+                if (b == 0xFF) p.Write(new byte[] { 0xFE }, 0, 1);
+            }
+            p.Write(new byte[] { crc }, 0, 1);
+            if (crc == 0xFF) p.Write(new byte[] { 0xFE }, 0, 1);
+            p.Write(new byte[] { 0xFF, 0xFF }, 0, 2);
+        }
+
+        private byte[] ReadFrame(SerialPort p)
+        {
+            try
+            {
+                while (p.ReadByte() != 0xFF) { }
+                var payload = new List<byte>();
+                while (true)
+                {
+                    int b = p.ReadByte();
+                    if (b == 0xFF)
+                    {
+                        int next = p.ReadByte();
+                        if (next == 0xFF) return payload.Count >= 3 ? payload.ToArray() : null;
+                        if (next == 0xFE) b = 0xFF;
+                        else { payload.Add(0xFF); payload.Add((byte)next); continue; }
+                    }
+                    else if (b == 0xFE) continue;
+                    payload.Add((byte)b);
+                }
+            }
+            catch { return null; }
+        }
+
+        private byte CalcCrc(byte b, byte crc)
+        {
+            byte x = (byte)(crc ^ b);
+            x = (byte)(x ^ (x << 4));
+            return (byte)(x ^ (x >> 3) ^ (x << 5));
+        }
+
+        // === Вспомогательные методы ===
+        private void LogInvoke(string text)
+        {
+            if (txtLog.InvokeRequired)
+                txtLog.Invoke((Action)(() => txtLog.AppendText(DateTime.Now.ToString("HH:mm:ss") + " " + text + "\r\n")));
+            else
+                txtLog.AppendText(DateTime.Now.ToString("HH:mm:ss") + " " + text + "\r\n");
+        }
+
+        private void log(string text) => LogInvoke(text);
+
+        // Сохранение/загрузка настроек
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.Port = cmbPort.Text;
+            Properties.Settings.Default.Baud = cmbBaud.Text;
+            Properties.Settings.Default.Address = myAddr;
+            Properties.Settings.Default.Serial = mySerial;
+            Properties.Settings.Default.Save();
+        }
+
+        private void LoadSettings()
+        {
+            cmbPort.Text = Properties.Settings.Default.Port;
+            cmbBaud.Text = Properties.Settings.Default.Baud;
+            numAddr.Value = Properties.Settings.Default.Address;
+            numSerial.Value = Properties.Settings.Default.Serial;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+            else
+            {
+                isRunning = false;
+                trayIcon.Visible = false;
+            }
+            SaveSettings();
+        }
+
+        // Обработчики изменения значений
+        private void myAddr_ValueChanged(object sender, EventArgs e) => myAddr = (byte)numAddr.Value;
+        private void numSerial_ValueChanged(object sender, EventArgs e) => mySerial = (byte)numSerial.Value;
+
+        private void numWeight_ValueChanged(object sender, EventArgs e) => currentWeight = (double)numWeight.Value;
+        private void numDecimals_ValueChanged(object sender, EventArgs e) => decimalPlaces = (int)numDecimals.Value;
+        private void chkStable_CheckedChanged(object sender, EventArgs e) => isStable = chkStable.Checked;
+        private void chkOverload_CheckedChanged(object sender, EventArgs e) => isOverload = chkOverload.Checked;
+        private void chkNegative_CheckedChanged(object sender, EventArgs e) => isNegative = chkNegative.Checked;
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            if (isRunning) return;
+
+            portName = cmbPort.Text;
+            baudRate = int.Parse(cmbBaud.Text);
+            myAddr = (byte)numAddr.Value;
+            mySerial = (uint)numSerial.Value;
+
+            isRunning = true;
+            workerThread = new Thread(SerialWorker) { IsBackground = true };
+            workerThread.Start();
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            log("Эмулятор запущен");
+        }
+
+        private void btnStop_Click_1(object sender, EventArgs e)
+        {
+            isRunning = false;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            log("Эмулятор остановлен");
+        }
+    }
+
+}
